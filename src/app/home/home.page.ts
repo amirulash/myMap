0c32d9053a77642a4505eee3a18b04cea272bcfa
amirulash <amirulfahmiash-shiddiqie@mail.ugm.ac.id>
2024-09-23 01:33:32 +0700

    Cuaca Radar

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { Geolocation } from '@capacitor/geolocation';
 import Map from '@arcgis/core/Map';
 import MapView from '@arcgis/core/views/MapView';
 import Graphic from '@arcgis/core/Graphic';
-import Point from '@arcgis/core/geometry/Point'; // Impor Point
-
+import Point from '@arcgis/core/geometry/Point';
+import SimpleMarkerSymbol from '@arcgis/core/symbols/SimpleMarkerSymbol';
+import ImageryLayer from '@arcgis/core/layers/ImageryLayer';
 
 @Component({
   selector: 'app-home',
@@ -12,58 +12,98 @@ import Point from '@arcgis/core/geometry/Point'; // Impor Point
   styleUrls: ['home.page.scss'],
 })
 export class HomePage implements OnInit {
-  private latitude: number | any;
-  private longitude: number | any;
+  mapView: MapView | any;
+  userLocationGraphic : Graphic | any;
+  selectedBasemap: string = "topo-vector";
 
-  constructor() {}
+  constructor () {}
 
-  public async ngOnInit() {
-    const position = await Geolocation.getCurrentPosition();
-    this.latitude = position.coords.latitude;
-    this.longitude = position.coords.longitude;
+  async ngOnInit() {
+    await this.initializeMap();
+    this.addWeatherPointMarkers();  // Menambahkan semua marker setelah peta diinisialisasi
+  }
 
-    // Buat instance peta
+
+  async initializeMap() {
     const map = new Map({
-      basemap: "satellite"
+      basemap: this.selectedBasemap
     });
 
-    const view = new MapView({
+    this.mapView = new MapView({
       container: "container",
       map: map,
-      zoom: 14,
-      center: [this.longitude, this.latitude] // Longitude, Latitude
+      zoom: 6, // Zoom level adjusted for better view of Kansas
+      center: [-97.5, 39.0] // Center map over Kansas
     });
 
-    // Gunakan class Point dari ArcGIS API
-    const point = new Point({
-      longitude: this.longitude,
-      latitude: this.latitude
+    let weatherServiceFL = new ImageryLayer({ url: WeatherServiceURL });
+    map.add(weatherServiceFL);
+
+    setInterval(this.updateUserLocationOnMap.bind(this), 10000);
+  }
+
+  async changeBasemap() {
+    if (this.mapView) {
+      this.mapView.map.basemap = this.selectedBasemap;
+    }
+  }
+
+  addWeatherPointMarkers() {
+    const locations = [
+      { longitude: -94.10610906794427,  latitude: 38.993325156826316  },  // Kansas
+      { longitude: -81.9115348156794, latitude: 36.2531368188833 },  // Los Angeles, California
+      { longitude: -87.6298, latitude: 41.8781 },  // Chicago, Illinois
+      { longitude: -122.4194, latitude: 37.7749 }  // San Francisco, California
+    ];
+
+    locations.forEach(location => {
+      // Create a point for each location
+      let point = new Point({
+        longitude: location.longitude,
+        latitude: location.latitude
+      });
+
+      // Create a symbol for the point
+      let markerSymbol = new SimpleMarkerSymbol({
+        color: [255, 0, 0], // Red color
+        size: '12px', // Size of the marker
+        outline: {
+          color: [255, 255, 255], // White outline
+          width: 2
+        }
+      });
+
+      // Create a graphic for each point and add it to the mapView
+      let pointGraphic = new Graphic({
+        geometry: point,
+        symbol: markerSymbol
+      });
+
+      this.mapView.graphics.add(pointGraphic);
     });
+  }
 
-    const markerSymbol = {
-      type: "simple-marker",
-      style: "circle", // Mengganti bentuk menjadi diamond (bisa juga 'circle', 'square', 'cross', dll.)
-      color: [255, 0, 0], // Warna biru cerah
-      size: "40px", // Ukuran marker
-      outline: {
-        color: [255, 255, 0], // Warna kuning pada garis tepi
-        width: 3
-      },
-      shadow: {
-        color: [50, 50, 50, 0.5], // Menambahkan bayangan dengan opasitas
-        offsetX: 3,
-        offsetY: 3,
-        blurRadius: 5
-      }
-    };
-
-
-    const pointGraphic = new Graphic({
-      geometry: point,  // Menggunakan class Point sebagai geometri
-      symbol: markerSymbol
+  async getLocationService(): Promise<number[]> {
+    return new Promise((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition((resp) => {
+        resolve([resp.coords.latitude, resp.coords.longitude]);
+      });
     });
+  }
 
-    // Tambahkan marker ke peta
-    view.graphics.add(pointGraphic);
+  async updateUserLocationOnMap() {
+    let latLng = await this.getLocationService();
+    let geom = new Point({ latitude: latLng[0], longitude: latLng[1] });
+    if (this.userLocationGraphic) {
+      this.userLocationGraphic.geometry = geom;
+    } else {
+      this.userLocationGraphic = new Graphic({
+          symbol: new SimpleMarkerSymbol(),
+          geometry: geom,
+      });
+      this.mapView.graphics.add(this.userLocationGraphic);
+    }
   }
 }
+
+const WeatherServiceURL = "https://mapservices.weather.noaa.gov/eventdriven/rest/services/radar/radar_base_reflectivity_time/ImageServer";
